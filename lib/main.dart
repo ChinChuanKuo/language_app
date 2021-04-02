0c25f9e3ac02c117df558d00bb111ccaa8210b93
ChinChuanKuo <chinchuankuo@ChinChuanKuos-MacBook-Pro.local>
2021-04-03 00:34:41 +0800

    module better

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:language_app/l10n/app_localization.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,16 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      localizationsDelegates: [
-        AppLocalizations.delegate, // Add this line
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        const Locale('en', ''), // English, no country code
-        const Locale('es', ''), // Spanish, no country code
-      ],
+      localizationsDelegates: AppLocalization.localizationsDelegates,
+      supportedLocales: AppLocalization.supportedLocales,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
